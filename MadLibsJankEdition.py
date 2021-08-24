8dc62e81b7a7dfe2d2db1f20641b482a5b9dd63c
Bryce Everett <70171126+BryceEverett@users.noreply.github.com>
2021-08-24 10:44:21 -0400

    Add files via upload

--- MadLibsJankEdition.py
+++ MadLibsJankEdition.py
@@ -1,4 +1,5 @@
 import random
+print('MADLIB1: He was a, adjective, Man that goes by, name, He is well known for the situation at, place,Where a/an , animal, was and he, verb, it', 'MADLIB2: The, animal, was roaming around the local, place,He caught the attention of the law enforcement by, verb, the nearby citizens. The police describe this animal as, adjective, and he goes by the name of, name ', 'MADLIB3: name, is the name of a man who does the unspeakable. He is known to , verb, at a/an local, place,He is usually seen with is pet, animal, that is describes as, adjective')
 adjective = input('Adjective: ')
 name = input('Name: ')
 place = input('Place: ')
